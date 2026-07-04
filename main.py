93a4227fc598b22f15d9fa41e7202527af1642f1
fliklylike-cyber <fliklylike@gmail.com>
2026-07-04 19:02:23 +0530

    Implement video generation and posting functionality
    Added functions to generate and post videos from stories.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from stories import generate_story
+from video import create_slides, create_video
+from telegram import send
+from facebook import post
 
+def run_once():
+    story = generate_story()
+
+    slides = create_slides(story)
+    video = create_video(slides)
+
+    send(video)
+    post(video)
+
+def run_batch(n=5):
+    for _ in range(n):
+        run_once()
+
+if __name__ == "__main__":
+    run_batch(3)  # number of videos
